23dbe68977b0ff85246e024b4710d828375a5594
William Woodall <wjwwood@gmail.com>
2013-01-04 17:19:31 -0800

    Update ogre.rb

--- ogre.rb
+++ ogre.rb
@@ -17,6 +17,8 @@ class Ogre < Formula
   def patches
     # https://gist.github.com/4237236
     [
+      "https://gist.github.com/raw/4237236/e357f1b9fa8b26d02ed84f411d5b5eb7446c68c5/pkg_config_fix.patch",
+      "https://gist.github.com/raw/4237236/57cb907304433cc0bb83fd332ff98a5789102b00/prevent_framework_apple.patch",
       "https://gist.github.com/raw/4237236/31ae53cefdb693cb2fb81333178163a29f8cf7ca/osx_isystem.patch",
       "https://gist.github.com/raw/4237236/9c7df6689da4e0b358429692f6615f2707469f45/osx_linking.patch",
       "https://gist.github.com/raw/4237236/d667813d5ee1e712e0ea8cc99df9a85da6141b1e/replace_pbxcp_with_ditto.patch"
