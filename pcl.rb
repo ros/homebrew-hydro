a451455005fcff29cf6beb12a0658b7c360ac082
William Woodall <william@osrfoundation.org>
2013-08-27 12:38:29 -0700

    Fixup pcl formula

--- pcl.rb
+++ pcl.rb
@@ -4,7 +4,7 @@ class Pcl < Formula
   homepage 'http://www.pointclouds.org'
   version '1.5.1'
   url 'https://github.com/PointCloudLibrary/pcl/archive/pcl-1.7.0.tar.gz'
-  sha1 ''
+  sha1 'b387f1ab218f545e3fe9f3c59d73b8fa8e2cf5ca'
 
   head 'https://github.com/PointCloudLibrary/pcl.git'
 
@@ -15,12 +15,12 @@ class Pcl < Formula
   depends_on 'vtk'
   depends_on 'qhull'
 
-  option '--with-debug', "Enable debug symbols"
+  option 'with-debug', "Enable debug symbols"
 
   def install
     args = std_cmake_parameters.split
 
-    if build.include? '--with-debug'
+    if build.with? 'debug'
       args << "-DCMAKE_BUILD_TYPE=Debug"
     end
 
