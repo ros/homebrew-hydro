3b01180c9667473d750a585e26ac258bef400031
jveitchmichaelis <j.veitchmichaelis+github@gmail.com>
2013-11-22 20:04:50 +0000

    Update pcl.rb
    Edited to update to vtk5 and pcl-1.7.1.  This is a HEAD-only fix at the moment, the stable version fails for some reason.
    
    I've included optional arguments for unit tests/examples and a check for sphinx-build.
    
    Also pcl apps are now prefixed 'pcl_foo', so updated the test.
    
    Checked on 10.8.4.

--- pcl.rb
+++ pcl.rb
@@ -1,33 +1,45 @@
 require 'formula'
 
 class Pcl < Formula
-  homepage 'http://www.pointclouds.org'
-  url 'https://github.com/PointCloudLibrary/pcl/archive/pcl-1.7.0.tar.gz'
-  sha1 'b387f1ab218f545e3fe9f3c59d73b8fa8e2cf5ca'
-
-  head 'https://github.com/PointCloudLibrary/pcl.git'
+  homepage 'http://pointclouds.org/'
+  url 'https://github.com/PointCloudLibrary/pcl/archive/pcl-1.7.1.tar.gz'
+  sha1 '784bce606141260423ea04f37b093f59d4c94c6a'
+  
+  head 'https://github.com/PointCloudLibrary/pcl.git', :revision => 'b1edb0d9cad7f33122398f2bea5d6339358bed5a'
 
   depends_on 'cmake' => :build
   depends_on 'boost'
   depends_on 'eigen'
   depends_on 'flann'
-  depends_on 'vtk'
-  depends_on 'qhull'
-
-  option 'with-debug', "Enable debug symbols"
-
+  depends_on 'vtk5' => 'with-qt'
+  depends_on 'qhull' => :recommended
+  depends_on 'sphinx' => :python
+   
+  option 'with-debug', "Enable debug symbols."
+  option 'with-tests', "Enable tests."
+  option 'with-examples', "Enable examples."
+  
   bottle do
     root_url 'http://download.ros.org/bottles'
     revision 1
     sha1 'f4547bff71a9cc32737df3f534c8071df605769e' => :mountain_lion
   end
-
+  
   def install
     args = std_cmake_parameters.split
 
     if build.with? 'debug'
       args << "-DCMAKE_BUILD_TYPE=Debug"
     end
+    
+    if build.with? 'tests'
+      args << "-DBUILD_global_tests=ON"
+    end
+    
+    if build.with? 'examples'
+      args << "-DBUILD_examples=ON"
+    end
+
 
     system "mkdir build"
     args << ".."
@@ -38,6 +50,6 @@ class Pcl < Formula
   end
 
   def test
-    system "bash -c 'echo | plyheader'"
+    system "bash -c 'echo | pcl_plyheader'"
   end
 end
